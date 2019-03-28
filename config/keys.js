42d43bdddc1903073643cabd0eea9b2d2a0ed05e
Emmanuel <sundayemmanuelabraham@gmail.com>
2019-03-28 15:37:55 +0100

    modified connection string password

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 const key = {
   // mongoURI: 'mongodb://localhost:27017/loginSystem',
-  mongoURI: 'mongodb+srv://emmanuel:<password>@cluster0-wnemk.mongodb.net/test?retryWrites=true',
+  mongoURI: 'mongodb+srv://emmanuel:login_system@cluster0-wnemk.mongodb.net/test?retryWrites=true',
   env: process.env.PORT || 5000
 };
 
