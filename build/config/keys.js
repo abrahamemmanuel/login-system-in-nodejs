3be4a8904864916bffc1674c70dba272fcc719d5
Emmanuel <sundayemmanuelabraham@gmail.com>
2019-04-14 21:12:43 +0100

    moved all js files to src folder

--- build/config/keys.js
+++ build/config/keys.js
@@ -5,8 +5,8 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 var key = {
-  mongoURI: 'mongodb://localhost:27017/loginSystem',
-  // mongoURI: 'mongodb+srv://emmanuel:login_system@cluster0-wnemk.mongodb.net/test?retryWrites=true',
+  // mongoURI: 'mongodb://localhost:27017/loginSystem',
+  mongoURI: 'mongodb+srv://emmy123:pwd123@cluster0-nw3ou.mongodb.net/test?retryWrites=true',
   // mongoURI: 'mongodb+srv://emmanuel:login_system@cluster0-wnemk.mongodb.net/test?retryWrites=true',
   env: process.env.PORT || 5000
 };
--- src/config/keys.js
+++ src/config/keys.js
@@ -1,6 +1,6 @@
 const key = {
   // mongoURI: 'mongodb://localhost:27017/loginSystem',
-  mongoURI: 'mongodb+srv://emmanuel:login_system@cluster0-wnemk.mongodb.net/test?retryWrites=true',
+  mongoURI: 'mongodb+srv://emmy123:pwd123@cluster0-nw3ou.mongodb.net/test?retryWrites=true',
   // mongoURI: 'mongodb+srv://emmanuel:login_system@cluster0-wnemk.mongodb.net/test?retryWrites=true',
   env: process.env.PORT || 5000
 };
