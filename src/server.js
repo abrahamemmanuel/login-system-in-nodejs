7a5798df1de5a2effa087ae3fda1b7d5d51fd4a0
Emmanuel <sundayemmanuelabraham@gmail.com>
2019-04-23 13:46:27 +0100

    changes

--- src/server.js
+++ src/server.js
@@ -55,7 +55,7 @@ mongoose.connect(db, { useNewUrlParser: true }).then(() => console.log('MongoDB 
 
 // if (!module.parent) { app.listen(key.env, () => console.log(`Server running on port ${key.env}`)); }// eslint-disable-line no-console
 
-app.get('/users/login', (req, res) => res.render('login'));
+app.get('/users/login', (req, res) => res.render('views/login'));
 
 app.listen(key.env, () => {console.log(`Server running on port ${key.env}`)});
 
