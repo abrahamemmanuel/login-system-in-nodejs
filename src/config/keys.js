6b4577c83fbdc98ad522d57c946f06ca9f1241da
Emmanuel <sundayemmanuelabraham@gmail.com>
2019-04-23 17:28:00 +0100

    modified

--- src/config/keys.js
+++ src/config/keys.js
@@ -2,6 +2,7 @@ const key = {
   LOCAL_URI: 'mongodb://localhost:27017/loginSystem',
   // mongoURI: 'mongodb+srv://emmy123:pwd123@cluster0-nw3ou.mongodb.net/test?retryWrites=true',
   // mongoURI: 'mongodb+srv://emmanuel:login_system@cluster0-wnemk.mongodb.net/test?retryWrites=true',
+  // MONGODB_URI: 'mongodb://heroku_91c65g66:b7tc9t2e05ajo8hrnt7o23qino@ds145315.mlab.com:45315/heroku_91c65g66',
   env: process.env.PORT || 5000
 };
 
